c46491a461986d24a6da9686604a401786d3f5d0
santhoshkumarramani <santhoshkumar37132@gmail.com>
2023-08-05 17:23:41 +0530

    new

---
+++ js/main.js
@@ -0,0 +1,34 @@
+//1.print N numbers of resverse order
+// let a=10;
+// for (let i=a ;i>=1 ; i--){
+	// console.log(i);
+// }
+
+ //2. print N odd numbers
+// let a=parseInt(prompt("enter the number"));
+ 
+ // for (let i=1; i<=a; i++){
+	 // if(i%2!=0)
+	 // console.log(i);
+ // }
+
+
+//3. Calculate sum of a number
+
+// let a=parseInt(prompt("enter the number"));
+// let b=parseInt(prompt("enter the number"));
+// c=a+b;
+// {
+	// console.log("total:"+c);
+// }
+
+// 4. Print Table of given number
+ // let a=parseInt(prompt("enter the number"));
+ // for ( let i=1;i<=10;i++){
+		// let temp=0;
+		// temp=temp+i
+	 // console.log(i+"x"+a+"="+temp*a);
+ // }
+ 
+
+ 
